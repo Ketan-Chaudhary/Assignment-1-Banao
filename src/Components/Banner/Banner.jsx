fa77f2b9ad5cb2ced61a4bcea6fb0edd278a7a81
Ketan Chaudhary <ketanchaudharypoonia@gmail.com>
2024-06-16 09:45:51 +0530

    Banner-refurbish

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,12 +1,41 @@
 import React from "react";
-import "./Banner.css";
 
 const Banner = () => {
   return (
-    <section className="innerWidth b-wrapper">
-      <span className="text-b">Computer Engineering</span>
-      <span className="text-b-s">142,765 Computer Engineers follow this</span>
-    </section>
+    <div class="relative  h-[28rem] text-white overflow-hidden mt-[0.7rem]">
+      <div class="absolute inset-0">
+        <img
+          src="banner.png"
+          alt="Background Image"
+          class="object-cover object-center w-full h-full"
+        />
+        <div class="absolute inset-0 bg-black opacity-50"></div>
+      </div>
+
+      <div
+        class="relative z-10 flex flex-col justify-end items-start h-full text-center"
+        style={{
+          marginLeft: "12rem",
+        }}
+      >
+        <h1
+          class="font-bold leading-tight mb-4"
+          style={{
+            fontSize: "36px",
+          }}
+        >
+          Computer Engineering
+        </h1>
+        <p
+          class="mb-8"
+          style={{
+            fontSize: "18px",
+          }}
+        >
+          142,765 Computer Engineers follow this
+        </p>
+      </div>
+    </div>
   );
 };
 
