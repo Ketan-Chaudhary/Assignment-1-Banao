c9b8905921ee7709d7a4aa003b1ca233d16e8293
Ketan Chaudhary <ketanchaudharypoonia@gmail.com>
2024-06-16 10:22:57 +0530

    Content Top refurbish

--- src/Components/ContentTop/ContentTop.jsx
+++ src/Components/ContentTop/ContentTop.jsx
@@ -13,14 +13,29 @@ import {
   Button,
 } from "@chakra-ui/react";
 import "./ContentTop.css";
+import { FaLocationDot } from "react-icons/fa6";
+import { Divider } from "@chakra-ui/react";
+import { MdErrorOutline } from "react-icons/md";
 import PostCard from "../PostCard/PostCard";
+import { RiArrowDownSFill } from "react-icons/ri";
+import { MdOutlineGroupAdd } from "react-icons/md";
 
 const ContentTop = () => {
   const tabName = ["All Post(32)", "Article", "Events", "Education", "Job"];
   return (
-    <section className="innerWidth top-wrapper flex">
-      <div className="w-full">
-        <Tabs variant="line" margin="2rem 200px">
+    <section
+      className="top-wrapper flex  "
+      style={{
+        margin: "2rem 200px",
+      }}
+    >
+      <div
+        className=""
+        style={{
+          maxWidth: "44rem",
+        }}
+      >
+        <Tabs variant="line">
           <TabList>
             {tabName.map((tab) => (
               <Tab
@@ -42,13 +57,92 @@ const ContentTop = () => {
               {" "}
               <PostCard />{" "}
             </TabPanel>
-            <TabPanel>Hellos</TabPanel>
+            <TabPanel>Working</TabPanel>
           </TabPanels>
         </Tabs>
       </div>
-      <div className="flex   gap-5 absolute top-ctn">
-        <button className="btn btn-ghost">Write a Post</button>
-        <button className="btn btn-ghost">Write a Post</button>
+      <div className="flex  flex-col  gap-5  top-ctn ">
+        <div
+          className="icons-post flex justify-end gap-[1rem]"
+          style={{
+            fontSize: "0.9rem",
+            wordSpacing: "0.2rem",
+          }}
+        >
+          <button className="btn btn-sm ">
+            Write a Post <RiArrowDownSFill />{" "}
+          </button>
+          <button
+            className="btn btn-sm  text-white"
+            style={{
+              background: "#2F6CE5",
+            }}
+          >
+            {" "}
+            <MdOutlineGroupAdd /> Join Group
+          </button>
+        </div>
+
+        <div
+          className="post-right"
+          style={{
+            marginLeft: "7rem",
+            marginTop: "4rem",
+          }}
+        >
+          <div className="post-right-static text-black ">
+            <label className=" flex items-center gap-2 text-black">
+              <FaLocationDot
+                className="opacity-70"
+                style={{
+                  width: "1.12rem",
+                }}
+              />
+              <input
+                type="text"
+                className="grow outline-none"
+                value="Noida, India"
+                style={{
+                  fontSize: "0.9rem",
+                }}
+              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="w-4 h-4 opacity-70"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </label>
+          </div>
+          <Divider className="mt-4" color="#B8B8B8" borderWidth={1.5} />
+          <div
+            className="flex gap-3 mt-[32px]"
+            style={{
+              color: "#8A8A8A",
+            }}
+          >
+            <MdErrorOutline
+              style={{
+                width: "1.2rem",
+              }}
+            />
+            <span
+              className=""
+              style={{
+                fontSize: "12px",
+              }}
+            >
+              Your location will help us serve better and extend a personalised
+              experience.
+            </span>
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/Components/PostCard/PostCard.jsx
+++ src/Components/PostCard/PostCard.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import CardLayout from "../Card/CardLayout";
-import { FaLocationDot } from "react-icons/fa6";
-import { Divider } from "@chakra-ui/react";
-import { MdErrorOutline } from "react-icons/md";
 
 const PostCard = () => {
   return (
@@ -10,52 +7,6 @@ const PostCard = () => {
       <div className="post-left">
         {" "}
         <CardLayout /> <CardLayout />{" "}
-      </div>
-      <div className="post-right w-[250px]">
-        <div className="post-right-static ">
-          <label className=" flex items-center gap-2 text-black">
-            <FaLocationDot className="w-4 h-4 opacity-70" />
-            <input
-              type="text"
-              className="grow outline-none"
-              placeholder="Search"
-            />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="w-4 h-4 opacity-70"
-            >
-              <path
-                fillRule="evenodd"
-                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </label>
-        </div>
-        <Divider className="mt-4" color="#B8B8B8" borderWidth={1.5} />
-        <div
-          className="flex gap-7 mt-[32px]"
-          style={{
-            color: "#8A8A8A",
-          }}
-        >
-          <MdErrorOutline
-            style={{
-              fontSize: "2.5rem",
-            }}
-          />
-          <span
-            className="w-[200px]"
-            style={{
-              fontSize: "12px",
-            }}
-          >
-            Your location will help us serve better and extend a personalised
-            experience.
-          </span>
-        </div>
       </div>
     </section>
   );
